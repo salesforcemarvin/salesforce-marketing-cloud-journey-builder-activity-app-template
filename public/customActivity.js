2d7ce969de7b56d730a19f8d5031b3d5cc3f66de
Henry Harion <hariondeveloper@gmail.com>
2024-11-24 21:37:13 +0700

    update code activity

--- public/customActivity.js
+++ public/customActivity.js
@@ -1,214 +1,237 @@
-define(["postmonger"], function (Postmonger) {
-  "use strict";
+define(["postmonger"], function(Postmonger) {
+    "use strict";
 
-  var connection = new Postmonger.Session();
-  var payload = {};
-  var lastStepEnabled = false;
-  var steps = [
-    // initialize to the same value as what's set in config.json for consistency
-    { label: "Step 1", key: "step1" },
-    { label: "Step 2", key: "step2" },
-    { label: "Step 3", key: "step3" },
-    { label: "Step 4", key: "step4", active: false },
-  ];
-  var currentStep = steps[0].key;
+    let channel = '@VCB_poc';
+    let contact = '632717898';
+    const token = '8091993565:AAE_BFhW4GU3e1702RlwdUTycr_DL1gOhBo';
+    const endpoint = 'https://api.telegram.org/bot';
+    const url = `${endpoint}${token}/`;
 
-  //Startup Sequence
-  $(window).ready(onRender);
+    let content = { 'message': '', 'photo': '' };
 
-  connection.on("initActivity", initialize);
-  connection.on("requestedTokens", onGetTokens);
-  connection.on("requestedEndpoints", onGetEndpoints);
+    var connection = new Postmonger.Session();
+    var payload = {};
+    var lastStepEnabled = false;
+    var steps = [
+        // initialize to the same value as what's set in config.json for consistency
+        { label: "Step 1", key: "step1" },
+        { label: "Step 2", key: "step2" },
+        { label: "Step 3", key: "step3" },
+        { label: "Step 4", key: "step4", active: false },
+    ];
+    var currentStep = steps[0].key;
 
-  connection.on("clickedNext", onClickedNext);
-  connection.on("clickedBack", onClickedBack);
-  connection.on("gotoStep", onGotoStep);
+    //Startup Sequence
+    $(window).ready(onRender);
 
-  function onRender() {
-    // JB will respond the first time 'ready' is called with 'initActivity'
-    connection.trigger("ready");
+    connection.on("initActivity", initialize);
+    connection.on("requestedTokens", onGetTokens);
+    connection.on("requestedEndpoints", onGetEndpoints);
 
-    connection.trigger("requestTokens");
-    connection.trigger("requestEndpoints");
+    connection.on("clickedNext", onClickedNext);
+    connection.on("clickedBack", onClickedBack);
+    connection.on("gotoStep", onGotoStep);
 
-    var message = getMessage();
-    $("#message").html(message);
+    function onRender() {
+        // JB will respond the first time 'ready' is called with 'initActivity'
+        connection.trigger("ready");
 
-  }
+        connection.trigger("requestTokens");
+        connection.trigger("requestEndpoints");
 
-  function initialize(data) {
+        var message = getMessage();
+        $("#message").html(message);
 
-    // document.getElementById("configuration").value = JSON.stringify(
-    //   data,
-    //   null,
-    //   2
-    // );
-    
-    console.log('-------- triggered:onInitActivity({obj}) --------');
-    if (data) {
-      payload = data;
     }
 
-    var message;
-    var hasInArguments = Boolean(
-      payload["arguments"] &&
-        payload["arguments"].execute &&
-        payload["arguments"].execute.inArguments &&
-        payload["arguments"].execute.inArguments.length > 0
-    );
+    function initialize(data) {
 
-    var inArguments = hasInArguments
-      ? payload["arguments"].execute.inArguments
-      : {};
-
-    $.each(inArguments, function (index, inArgument) {
-      $.each(inArgument, function (key, val) {
-        if (key === "message") {
-          message = val;
+        console.log('-------- triggered:onInitActivity({obj}) --------');
+        if (data) {
+            payload = data;
         }
-      });
-    });
 
-    
+        var message;
+        var hasInArguments = Boolean(
+            payload["arguments"] &&
+            payload["arguments"].execute &&
+            payload["arguments"].execute.inArguments &&
+            payload["arguments"].execute.inArguments.length > 0
+        );
 
-    // If there is no message selected, disable the next button
-    if (!message) {
-      showStep(null, 1);
-      connection.trigger("updateButton", { button: "next", enabled: true });
-      //connection.trigger("updateButton", { button: "next", enabled: false });
-      // If there is a message, skip to the summary step
-    } else {
-      
-      document.getElementById("configuration").value = message;
-      $("#message").html(message);
-      //showStep(null, 3);
-    }
-  }
+        var inArguments = hasInArguments ?
+            payload["arguments"].execute.inArguments : {};
 
-  function onGetTokens(tokens) {
-    // Response: tokens = { token: <legacy token>, fuel2token: <fuel api token> }
-    // console.log(tokens);
-  }
-
-  function onGetEndpoints(endpoints) {
-    // Response: endpoints = { restHost: <url> } i.e. "rest.s1.qa1.exacttarget.com"
-    // console.log(endpoints);
-  }
-
-  //Save Sequence
-  function onClickedNext() {
-    // var configuration = JSON.parse(
-    //   document.getElementById("configuration").value
-    // );
-    //var configuration = document.getElementById("configuration").value;
-    
-    //connection.trigger("updateActivity", configuration);
-    //save(configuration);
-
-    if (
-      (currentStep.key === "step3" && steps[3].active === false) ||
-      currentStep.key === "step4"
-    ) {
-      save();
-    } else {
-      connection.trigger("nextStep");
-    }
-  }
-
-  function onClickedBack() {
-    connection.trigger("prevStep");
-  }
-
-  function onGotoStep(step) {
-    showStep(step);
-    connection.trigger("ready");
-  }
-
-  function showStep(step, stepIndex) {
-    if (stepIndex && !step) {
-      step = steps[stepIndex - 1];
-    }
-
-    currentStep = step;
-
-    $(".step").hide();
-
-    switch (currentStep.key) {
-      case "step1":
-        $("#step1").show();
-        connection.trigger("updateButton", {
-          button: "next",
-          enabled: true, //Boolean(getMessage()),
+        $.each(inArguments, function(index, inArgument) {
+            $.each(inArgument, function(key, val) {
+                if (key === "message") {
+                    message = val;
+                }
+            });
         });
-        connection.trigger("updateButton", {
-          button: "back",
-          visible: false,
-        });
-        break;
-      case "step2":
-        $("#step2").show();
-        connection.trigger("updateButton", {
-          button: "back",
-          visible: true,
-        });
-        connection.trigger("updateButton", {
-          button: "next",
-          text: "next",
-          visible: true,
-        });
-        break;
-      case "step3":
-        $("#step3").show();
-        connection.trigger("updateButton", {
-          button: "back",
-          visible: true,
-        });
-        if (lastStepEnabled) {
-          connection.trigger("updateButton", {
-            button: "next",
-            text: "next",
-            visible: true,
-          });
+
+
+
+        // If there is no message selected, disable the next button
+        if (!message) {
+            showStep(null, 1);
+            connection.trigger("updateButton", { button: "next", enabled: true });
+            //connection.trigger("updateButton", { button: "next", enabled: false });
+            // If there is a message, skip to the summary step
         } else {
-          connection.trigger("updateButton", {
-            button: "next",
-            text: "done",
-            visible: true,
-          });
+
+            document.getElementById("configuration").value = message;
+            $("#message").html(message);
+            //showStep(null, 3);
         }
-        break;
-      case "step4":
-        $("#step4").show();
-        break;
     }
-  }
 
-  function save() {
-    
-      var value = getMessage();
+    function onGetTokens(tokens) {
+        // Response: tokens = { token: <legacy token>, fuel2token: <fuel api token> }
+        // console.log(tokens);
+    }
 
-      // 'payload' is initialized on 'initActivity' above.
-      // Journey Builder sends an initial payload with defaults
-      // set by this activity's config.json file.  Any property
-      // may be overridden as desired.
-      payload.name = value; //text message to send to telegram
+    function onGetEndpoints(endpoints) {
+        // Response: endpoints = { restHost: <url> } i.e. "rest.s1.qa1.exacttarget.com"
+        // console.log(endpoints);
+    }
 
-      payload["arguments"].execute.inArguments = [{ message: value }];
-      //payload["arguments"].execute.inArguments = [{ "chat_id": "@vcbsalesforce", "text": value }];
-  
-      payload["metaData"].isConfigured = true;
+    //Save Sequence
+    function onClickedNext() {
+        // var configuration = JSON.parse(
+        //   document.getElementById("configuration").value
+        // );
+        //var configuration = document.getElementById("configuration").value;
+
+        //connection.trigger("updateActivity", configuration);
+        //save(configuration);
+
+        if (
+            (currentStep.key === "step3" && steps[3].active === false) ||
+            currentStep.key === "step4"
+        ) {
+            save();
+        } else {
+            connection.trigger("nextStep");
+        }
+    }
+
+    function onClickedBack() {
+        connection.trigger("prevStep");
+    }
+
+    function onGotoStep(step) {
+        showStep(step);
+        connection.trigger("ready");
+    }
+
+    function showStep(step, stepIndex) {
+        if (stepIndex && !step) {
+            step = steps[stepIndex - 1];
+        }
+
+        currentStep = step;
+
+        $(".step").hide();
+
+        switch (currentStep.key) {
+            case "step1":
+                $("#step1").show();
+                connection.trigger("updateButton", {
+                    button: "next",
+                    enabled: true, //Boolean(getMessage()),
+                });
+                connection.trigger("updateButton", {
+                    button: "back",
+                    visible: false,
+                });
+                break;
+            case "step2":
+                $("#step2").show();
+                connection.trigger("updateButton", {
+                    button: "back",
+                    visible: true,
+                });
+                connection.trigger("updateButton", {
+                    button: "next",
+                    text: "next",
+                    visible: true,
+                });
+                break;
+            case "step3":
+                $("#step3").show();
+                connection.trigger("updateButton", {
+                    button: "back",
+                    visible: true,
+                });
+                if (lastStepEnabled) {
+                    connection.trigger("updateButton", {
+                        button: "next",
+                        text: "next",
+                        visible: true,
+                    });
+                } else {
+                    connection.trigger("updateButton", {
+                        button: "next",
+                        text: "done",
+                        visible: true,
+                    });
+                }
+                break;
+            case "step4":
+                $("#step4").show();
+                break;
+        }
+    }
+
+    function call(content) {
+        if (content.message !== '') {
+            let data = {
+                'chat_id': channel,
+                'text': content.message
+            };
+
+            if (content.photo !== '') {
+                data.photo = content.photo;
+            }
+
+            $.ajax({
+                url: url,
+                data: data,
+                dataType: 'json',
+                success: (res) => {
+                    console.log(res.json());
+                }
+            });
+        }
+    }
+
+    function save() {
+
+        var value = getMessage();
+
+        // 'payload' is initialized on 'initActivity' above.
+        // Journey Builder sends an initial payload with defaults
+        // set by this activity's config.json file.  Any property
+        // may be overridden as desired.
+        payload.name = value; //text message to send to telegram
+
+        payload["arguments"].execute.inArguments = [{ message: value }];
+        //payload["arguments"].execute.inArguments = [{ "chat_id": "@vcbsalesforce", "text": value }];
+
+        payload["metaData"].isConfigured = true;
 
 
-      // get the option that the user selected and save it to
-      console.log('------------ triggering:updateActivity({obj}) ----------------');
-      console.log('Sending message back to updateActivity');
-      console.log('saving\n', value);
-      console.log('--------------------------------------------------------------');
-      connection.trigger("updateActivity", payload);
+        // get the option that the user selected and save it to
+        console.log('------------ triggering:updateActivity({obj}) ----------------');
+        console.log('Sending message back to updateActivity');
+        console.log('saving\n', value);
+        console.log('--------------------------------------------------------------');
+        connection.trigger("updateActivity", payload);
 
-  }
+    }
 
-  function getMessage() {
-    return document.getElementById("configuration").value;
-  }
+    function getMessage() {
+        return document.getElementById("configuration").value;
+    }
 });
