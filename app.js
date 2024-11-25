02dad37b4ac79925f28a0347fc5cdd5171789d6c
salesforcemarvin <mlacuna@salesforce.com>
2024-11-25 18:07:07 +0800

    Update app.js

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.post('/logout', routes.logout );
 app.post('/journeybuilder/save/', activity.save );
 app.post('/journeybuilder/validate/', activity.validate );
 app.post('/journeybuilder/publish/', activity.publish );
-app.post('/journeybuilder/execute/', activity.execute );
+app.post('/api/execute/', activity.execute );
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('SFMC - JB - Activity App is listening on port ' + app.get('port'));
